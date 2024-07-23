ddfe8ee50b38663ee5f735196eda4f4d4e76be8b
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-07-23 14:26:56 +0900

    first commit

---
+++ src/main/java/com/be/fitminder/FitMinderApplication.java
@@ -0,0 +1,13 @@
+package com.be.fitminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitMinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitMinderApplication.class, args);
+    }
+
+}

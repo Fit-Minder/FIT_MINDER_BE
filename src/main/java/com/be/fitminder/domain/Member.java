62f0492ce9bf5af2354e8b799c648d206c27efc3
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-07-31 15:04:21 +0900

    refactor: BaseEntity 추가 & Grass 연관관계 매핑

--- src/main/java/com/be/fitminder/domain/Member.java
+++ src/main/java/com/be/fitminder/domain/Member.java
@@ -1,14 +1,19 @@
 package com.be.fitminder.domain;
 
+import com.be.fitminder.config.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +25,19 @@ public class Member {
 
     private String role;
 
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Grass> grasses = new ArrayList<>();
+
     public void changeName(String name) {
         this.name = name;
     }
+
+//    public Member(Long id, String username, String name, String role) {
+//        this.id = id;
+//        this.username = username;
+//        this.name = name;
+//        this.role = role;
+//    }
+
+
 }

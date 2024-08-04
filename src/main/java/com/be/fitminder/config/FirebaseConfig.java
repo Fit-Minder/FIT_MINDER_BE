b711eb23fe6a0c7a5a02fcb75db462086def3451
supportlaver <ak4686@naver.com>
2024-08-04 18:45:07 +0900

    chore: 배포 환경 스크립트 수정

--- src/main/java/com/be/fitminder/config/FirebaseConfig.java
+++ src/main/java/com/be/fitminder/config/FirebaseConfig.java
@@ -3,22 +3,43 @@ package com.be.fitminder.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
+@RequiredArgsConstructor
 public class FirebaseConfig {
+
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucketName;
+    private String key = "fitminderfcm-firebase-adminsdk-pz1u6-a788b6075b.json";
 
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        FileInputStream serviceAccount =
-                new FileInputStream("src/main/resources/firebase/firebase_service_key.json");
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        ResponseBytes<GetObjectResponse> objectAsBytes = s3Client.getObjectAsBytes(getObjectRequest);
+
+        InputStream inputStream = new ByteArrayInputStream(objectAsBytes.asByteArray());
 
         FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(inputStream))
                 .build();
 
         return FirebaseApp.initializeApp(options);

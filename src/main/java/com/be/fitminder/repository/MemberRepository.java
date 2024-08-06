52145dcc67ed873b4cef8c38245f84d30848aa12
namkikim0718 <113903598+namkikim0718@users.noreply.github.com>
2024-08-06 21:57:54 +0900

    refactor: 로그인 성공 시 로직 수정

--- src/main/java/com/be/fitminder/repository/MemberRepository.java
+++ src/main/java/com/be/fitminder/repository/MemberRepository.java
@@ -3,7 +3,9 @@ package com.be.fitminder.repository;
 import com.be.fitminder.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByUsername(String username);
+    Optional<Member> findByUsername(String username);
 }
